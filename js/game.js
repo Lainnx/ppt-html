e64c45b8570db62083e769c1c46be3202f395a29
das <tree@gmail.com>
2025-04-08 19:19:20 +0200

    d

--- js/game.js
+++ js/game.js
@@ -5,6 +5,12 @@ let pNombre = document.querySelector("header p");    //el p dentro del header
 let respuesta = document.getElementById("respuesta")
 let divImagen = document.getElementById("divImagen")
 
+let victorias = 0
+let empates = 0
+let derrotas = 0
+
+let iconos=["✊","🖐","✌"]
+
 let nombreUsuario =""
 
 buttonNombre.addEventListener("click",()=>{
@@ -30,21 +36,42 @@ formJuego.addEventListener("submit",(e)=>{
     let audioEmpate = document.getElementById("empate");
     let audioDerrota = document.getElementById("derrota");
 
+    mensaje+=`<p>Tu jugada ha sido: ${iconos[jugadaHumano-1]}</p>
+        <p>Mi jugada ha sido: ${iconos[jugadaPC-1]}</p>`
+      
+
     if(jugadaHumano == jugadaPC){
-        mensaje="¡ Empate !";
+        mensaje+=`<p>¡ Habéis empatado ${nombreUsuario}!</p>`;
         audioEmpate.play();
         divImagen.innerHTML="<img src='/img/empate.jpg' alt='empate'>";
+        empates++
     }else if((jugadaHumano==1 && jugadaPC==3)||(jugadaHumano==2 && jugadaPC==1)||(jugadaHumano==3 && jugadaPC==2)){
-        mensaje=`¡ Has ganado ${nombreUsuario}!`;
+        mensaje+=`<p>¡ Has ganado ${nombreUsuario}!</p>`;
         audioVictoria.play();
         divImagen.innerHTML="<img src='/img/victoria.jpg' alt='victoria'>";
+        victorias++
     }else{
-        mensaje=`¡ Has perdido ${nombreUsuario}!`;
+        mensaje+=`<p>¡ Has perdido ${nombreUsuario}!</p>`;
         audioDerrota.play();
         divImagen.innerHTML="<img src='/img/derrota.jpg' alt='derrota'>";
+        derrotas++
     }
-
-    respuesta.innerHTML = `<p>${mensaje}</p>`
+    // mensaje+=`<p>Victorias: ${victorias} | Derrotas: ${derrotas} | Empates: ${empates} | ${(victorias/(derrotas+empates)).toFixed(2)}</p>`
+    let tabla = `    
+    <table>
+        <tr>
+            <th>Victorias</th>
+            <th>Empates</th>
+            <th>Derrotas</th>
+        </tr>
+        <tr>
+            <td>${victorias}</td>
+            <td>${empates}</td>
+            <td>${derrotas}</td>
+        </tr>
+    </table>`
+    // mensaje += tabla
+    respuesta.innerHTML = `<p>${mensaje}</p>${tabla}`
 
 })
 
